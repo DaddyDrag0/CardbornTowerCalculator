0ec515018f6d21bbf6c03879d23532fa496565f2
Drago <dragoidc3@gmail.com>
2026-09-04 11:25:33 -0400

    Bump experimental child worker to v48

--- sim-child-exp.js
+++ sim-child-exp.js
@@ -1,6 +1,6 @@
 // Thin wrapper around the stable single-run worker.
 // Captures the full requested ban list (up to 10) for Experimental mode.
-importScripts('./sim-child.js?v=47');
+importScripts('./sim-child.js?v=48');
 const EXP_CHILD_baseOnMessage=self.onmessage;
 self.onmessage=e=>{
   try{self.__cardbornAllBans=Array.isArray(e?.data?.bans)?e.data.bans.slice(0,10):[]}catch(_){self.__cardbornAllBans=[]}
