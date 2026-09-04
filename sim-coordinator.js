19d6898bbcec23f8a63175a476330d22b807db0c
Drago <dragoidc3@gmail.com>
2026-09-04 10:48:56 -0400

    Bump coordinator child worker to v47

--- sim-coordinator.js
+++ sim-coordinator.js
@@ -3,7 +3,7 @@
 // the next queued run to whichever child finishes first.
 
 const MAX_WORKERS=8;
-const CHILD_URL='./sim-child-exp.js?v=46';
+const CHILD_URL='./sim-child-exp.js?v=47';
 
 self.onmessage=e=>{
   const req=e.data||{};
